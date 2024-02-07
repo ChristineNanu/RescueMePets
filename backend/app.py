b872c2bd8a66d4a26811080335ac6349f8f409d8
christinemundi <mundichristine@gmail.com>
2024-02-07 11:19:27 +0300

    flask installed

---
+++ backend/app.py
@@ -0,0 +1,10 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
