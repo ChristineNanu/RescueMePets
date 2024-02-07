1d9eaee0ef3d9eff9c83fcb2bad3718e57160a81
christinemundi <mundichristine@gmail.com>
2024-02-07 15:12:51 +0300

    models created

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    """A user that can login to the app."""
+    
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(80), unique=True, nullable=False)
+    password = Column(String(200), nullable=False) 
+    
